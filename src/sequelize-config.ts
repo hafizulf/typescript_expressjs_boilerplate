b022f19ab56622c2c34efd3e01e56a2c71680997
Hafizul Furqan <hafizulf68@gmail.com>
2025-09-19 18:51:00 +0700

    fix: sequelize config

--- src/sequelize-config.ts
+++ src/sequelize-config.ts
@@ -1,4 +1,4 @@
-require("dotenv").config();
+import "dotenv/config";
 
 const common = {
   username: process.env.DB_USER,
@@ -6,12 +6,14 @@ const common = {
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT || 5432),
-  dialect: "postgres",
+  dialect: "postgres" as const,
   logging: false,
 };
 
-module.exports = {
+const config = {
   development: { ...common },
   test: { ...common, database: process.env.DB_DATABASE_TEST || "typescript_expressjs_boilerplate_test" },
   production: { ...common },
 };
+
+export = config;
